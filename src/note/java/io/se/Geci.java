8841eed1926ff6d19b80a0d95c04e27faa931eb2
lihonggit <517303073@qq.com>
2016-06-15 18:28:49 +0800

    m

--- src/note/java/io/se/Geci.java
+++ src/note/java/io/se/Geci.java
@@ -8,7 +8,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import note.java.util.regex._RegexUtils;
 
