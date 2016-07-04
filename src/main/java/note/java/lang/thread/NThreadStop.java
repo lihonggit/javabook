e97a5ad764d52b08c673a9769982450dde567b8c
lihonggit <517303073@qq.com>
2016-07-04 18:32:59 +0800

    thread

---
+++ src/main/java/note/java/lang/thread/NThreadStop.java
@@ -0,0 +1,10 @@
+package note.java.lang.thread;
+
+public class NThreadStop {
+	public static void main(String[] args) {
+		System.out.println("今天，");
+		for (int i = 0; i < 100; i++) {
+			System.out.println("我");
+		}
+	}
+}
